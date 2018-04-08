8b2d651fa7f7f1b8f8f1755a4d4affce2f4f2bf3
Zach Hall <zachvac@gmail.com>
2018-04-08 01:28:15 -0400

    server should be functioning now

--- readme.md
+++ readme.md
@@ -1,3 +1,41 @@
 # Node Server #
 
 I'm using this to learn node.js, but also to build a functioning web server I can use to host a variety of projects.
+
+## Format of Server ##
+### To server up a static web page ###
+server/folder/www/path/page.extension
+
+### To call a node.js function###
+server/folder/data/functionName?arg1=1&arg2=2...
+
+### folderLookup.txt ###
+a JSON with folder names mapped to server locations.
+If there is no mapping the server looks for the folder
+in the same location as the server.js file
+
+### node.js functions ###
+The folder must have an index.js with the following:
+```
+exports.export = function(funcName,query){
+	if(this[funcName]){
+		return(this[funcName](query));
+	}
+	return("ERR, "+funcName+" does not exist");
+}
+```
+Functions may then be defined as follows:
+```
+exports.[functionName] = function(query){
+  ...
+}
+```
+For example, here is a function called add, which returns the sum of arguments
+n1 and n2. If this were in a folder called zach it would be called by:
+server/zach/data/add?n1=5&n2=7
+
+```
+exports.add = function(query){
+	return((Number(query.n1)+Number(query.n2)).toString());
+}
+```
--- server.js
+++ server.js
@@ -2,40 +2,66 @@ const mysql = require('mysql');
 const url = require('url');
 const http = require('http');
 const fs = require('fs');
-
-const pw = fs.readFileSync('pw.txt')
-
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "zachvac",
-  password: pw
-});
-const favicon = fs.readFileSync('C:\\Users\\Zach\\ez\\favicon.jpg');
-con.connect(function(err) {
-  if (err) throw err;
-});
+const port = 80;
+const folderLookup = JSON.parse(fs.readFileSync("folderLookup.txt").toString());
 http.createServer(function (req, res) {
-  console.log("Connected");
-  if(req.url == "/favicon.ico"){
-    res.writeHead(200,{'Content-Type':'image/jpeg'})
-    res.end(favicon,"binary");
-  }else{
+  const path = url.parse(req.url,true).pathname;
+  const folder = path.split("/")[1];
+  let folderLocation = "./"+folder;
+  if(folderLookup[folder]){folderLocation = folderLookup[folder];}
+  if(path.split("/")[2]=="www"){
+    console.log("www");
+    const pathWithoutSlash = path.substr(1);
+    const pathWithoutFolder = pathWithoutSlash.substr(pathWithoutSlash.indexOf("/"));
+    fs.readFile(folderLocation+pathWithoutFolder, function(err, data) {
+      if(err){
+        res.writeHead(404,{'content-Type':'text/html'});
+        res.write("404 File Not Found");
+        res.end();
+      }else{
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      }
+    });
+  }else if(path.split("/")[2]=="data"){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    var query = url.parse(req.url, true).query.query;
-    res.write(JSON.stringify(req.headers)+"<br/>");
-    res.write(req.url);
-    res.end();
-  }//end of else
-  }).listen(80);
+    try{
+      const dat = require(folderLocation);
+      res.write(dat.export(path.split("/")[3],url.parse(req.url,true).query));
+      res.end();
+    }catch(err){
+      res.writeHead(404,{'content-Type':'text/html'});
+      res.write("404 File Not Found");
+      res.end();
+    }
+  }else{
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end("Not really sure what you're trying to do here.");
+  }
+}).listen(port);
 
 
 
 /*
 if(query){
-  con.query(query,function(err,result){
-    if (err) throw err;
-    res.write(JSON.stringify(result));
-    res.end();
-  });
+con.query(query,function(err,result){
+if (err) throw err;
+res.write(JSON.stringify(result));
+res.end();
+});
 }
+
+
+con.connect(function(err) {
+if (err) throw err;
+});
+
+const pw = fs.readFileSync('pw.txt')
+
+const con = mysql.createConnection({
+host: "localhost",
+user: "zachvac",
+password: pw
+});
 */
